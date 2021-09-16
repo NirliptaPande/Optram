8d63af4a699e6dd3e096d40c0ff8bf1f3363f2f3
NirliptaPande <nirliptapande@gmail.com>
2021-09-16 14:45:42 +0530

    Some edits in soil remaining

--- soil.py
+++ soil.py
@@ -12,11 +12,17 @@ def expreg(X, y):
     pred99 = gam99.predict(X_arr)
     pred005 = gam005.predict(X_arr)
     return max(pred99), max(pred005)
-length = (len(band4)//300)+1
-wet_final = [length][length]
-dry_final = [length][length]
-for len1 in in range(0, len(band4), 300):
-    for len2 in range(0, len(band4), 300):
+files = os.listdir('data/')
+band4 = [file for file in files if re.match(pattern1, file)]
+temp_file = mpimg.imread('./data/' + band4[0])
+row = temp_file.shape[0]
+col = temp_file.shape[1]
+len_row = (row//300)+1
+len_col = (col//300)+1
+wet_final = [len_row][len_col]
+dry_final = [len_row][len_col]
+for len1 in in range(0, row, 300):
+    for len2 in range(0, col, 300):
         data = pd.read_csv("data%%.csv"%(len1,len2))
         ndvi = data['0.0']
         svr = data['0.0.1']
@@ -26,8 +32,8 @@ for len1 in in range(0, len(band4), 300):
 for i in range(35):
     #soil%i[len(band4)][len(band4)]
     #basically create 35 arrays
-for len1 in in range(0, len(band4), 300):
-    for len2 in range(0, len(band4), 300):
+for len1 in in range(0, row, 300):
+    for len2 in range(0, col, 300):
         data = pd.read_csv("data%%.csv"%(len1,len2))
         wet = wet_final[len1//300][len2//300]
         dry = dry_final[len1//300][len2//300]
