4b3fb4b1258edce212443adc1a1c0d79957881bc
NirliptaPande <nirliptapande@gmail.com>
2021-09-16 15:49:26 +0530

    Pep8 the code

--- soil.py
+++ soil.py
@@ -1,4 +1,9 @@
 from pygam import ExpectileGAM
+import pandas as pd
+import os
+import re
+import matplotlib.image as mpimg
+
 
 def expreg(X, y):
     X_arr = X.to_numpy()
@@ -12,34 +17,44 @@ def expreg(X, y):
     pred99 = gam99.predict(X_arr)
     pred005 = gam005.predict(X_arr)
     return max(pred99), max(pred005)
+
+
 files = os.listdir('data/')
+pattern1 = '^s2tile_31UDR_R051-N28_stack_s2-B04_2018.....tif$'
 band4 = [file for file in files if re.match(pattern1, file)]
 temp_file = mpimg.imread('./data/' + band4[0])
 row = temp_file.shape[0]
 col = temp_file.shape[1]
-len_row = (row//300)+1
-len_col = (col//300)+1
+
+len_row = (row // 300) + 1
+len_col = (col // 300) + 1
+
 wet_final = [len_row][len_col]
 dry_final = [len_row][len_col]
-for len1 in in range(0, row, 300):
+for len1 in range(0, row, 300):
     for len2 in range(0, col, 300):
-        data = pd.read_csv("data%%.csv"%(len1,len2))
+        data = pd.read_csv("data%%.csv" % (len1, len2))
         ndvi = data['0.0']
         svr = data['0.0.1']
         del data
         print('calling expecreg')
-        wet_final[len1//300][len2//300], dry_final[len1//300][len2//300] = expreg(ndvi, svr)
+        (
+            wet_final[len1 // 300][len2 // 300],
+            dry_final[len1 // 300][len2 // 300],
+        ) = expreg(ndvi, svr)
 for i in range(35):
-    #soil%i[len(band4)][len(band4)]
-    #basically create 35 arrays
-for len1 in in range(0, row, 300):
+    pass
+    # soil%i[len(band4)][len(band4)]
+    # basically create 35 arrays
+
+for len1 in range(0, row, 300):
     for len2 in range(0, col, 300):
-        data = pd.read_csv("data%%.csv"%(len1,len2))
+        data = pd.read_csv("data%%.csv" % (len1, len2))
         svr = data['0.0.1']
-        wet = wet_final[len1//300][len2//300]
-        dry = dry_final[len1//300][len2//300]
-        data = (svr-dry)/(wet-dry)
-        #split it into 35 parts 
-        #reshape each into a 300 by 300
-        #place each colum in the appropriate place in the larger array
-#convert it all into raster files
+        wet = wet_final[len1 // 300][len2 // 300]
+        dry = dry_final[len1 // 300][len2 // 300]
+        data = (svr - dry) / (wet - dry)
+        # split it into 35 parts
+        # reshape each into a 300 by 300
+        # place each colum in the appropriate place in the larger array
+# convert it all into raster files
