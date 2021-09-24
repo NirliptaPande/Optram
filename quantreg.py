916faee0d280ff8567998e4f893fa3e1cc3cbfbb
NirliptaPande <nirliptapande@gmail.com>
2021-09-24 15:22:43 +0530

    QuantReg

--- quantreg.py
+++ quantreg.py
@@ -10,8 +10,8 @@ import pandas as pd
 
 
 def quantile_reg(df_ndvi, df_svr1):
-    data = pd.concat([df_ndvi, df_svr1], axis =1)
-#    print(data.head())
+    data = pd.concat([df_ndvi, df_svr1], axis=1)
+    #    print(data.head())
     data.columns = ['x', 'y']
     indices = data.isin([np.nan, np.inf, -np.inf]).any(1)
     data = data[~indices]
@@ -25,19 +25,19 @@ def quantile_reg(df_ndvi, df_svr1):
     params = []
     params1 = []
     for qt in quantiles:
-        res1 = mod1.fit(q = qt)
-        res = mod.fit(q = qt)
+        res1 = mod1.fit(q=qt)
+        res = mod.fit(q=qt)
         models.append(res)
         models1.append(res1)
         params1.append([qt, res1.params['Intercept'], res1.params['y']])
         params.append([qt, res.params['Intercept'], res.params['x']])
 
-    params = pd.DataFrame(data = params, columns = ['qt', 'intercept', 'x_coef'])
-    params1 = pd.DataFrame(data = params1, columns = ['qt', 'intercept', 'x_coef'])
+    params = pd.DataFrame(data=params, columns=['qt', 'intercept', 'x_coef'])
+    params1 = pd.DataFrame(data=params1, columns=['qt', 'intercept', 'x_coef'])
 
-#    print(models[0].summary())
+    #    print(models[0].summary())
 
-    #res = model1.fit(q=.95)
+    # res = model1.fit(q=.95)
     plt.scatter(df_ndvi, df_svr1, color='black', marker='x', alpha=0.02)
     y_pred1 = models[0].params['Intercept'] + models[0].params['x'] * df_ndvi
     plt.plot(df_ndvi, y_pred1, linewidth=2, label='Q Reg : 0.05')
@@ -60,11 +60,15 @@ def quantile_reg(df_ndvi, df_svr1):
     plt.title("NDVI- STR scatter plot", fontsize=20)
     plt.xlabel("NDVI", fontsize=15)
     plt.ylabel("STR", fontsize=15)
-    plt.savefig("%sNDVI-STR.jpg" % ii)
+    plt.savefig("clefinal_NDVI-STR.jpg")
     plt.close()
 
-
     # i=i+1
+
+
+final_svr = np.array([])
+final_ndvi = np.array([])
+
 pattern1 = '^s2tile_31UDR_R051-N28_stack_s2-B04_2018.....tif$'
 pattern2 = '^s2tile_31UDR_R051-N28_stack_s2-B08_2018.....tif$'
 swir11 = '^s2tile_31UDR_R051-N28_stack_s2-B11_2018.....tif$'
@@ -95,7 +99,7 @@ for file1, file2, file3, file4 in zip(band4, band8, band11, band12):
     img4.astype('float64')
     img12.astype('float64')
     img11.astype('float64')
-#    x = img8 + img4 
+    #    x = img8 + img4
     ragh = []
     test1 = []
     test2 = []
@@ -106,42 +110,24 @@ for file1, file2, file3, file4 in zip(band4, band8, band11, band12):
         ragh.append(temp)
         t3 = np.copy(img11[i].astype('float64'))
         t4 = np.copy(img12[i].astype('float64'))
-        temp1 = np.true_divide(((1 - t3)**2), (2 * t3))
-        temp2 = np.true_divide(((1 - t4)**2), (2 * t4))
+        temp1 = np.true_divide(((1 - t3) ** 2), (2 * t3))
+        temp2 = np.true_divide(((1 - t4) ** 2), (2 * t4))
         test1.append(temp1)
         test2.append(temp2)
     ndvi = np.array(ragh)
-    svr1 = np.array(test1)
     svr2 = np.array(test2)
-    #ndvi = np.divide(img8 - img4, x)
-    invalid = (ndvi > 1).any()
-    if invalid:
-        pdb.set_trace()
-
-#    img11 = mpimg.imread('./data/'+file3)
- #   img12 = mpimg.imread('./data/'+file4)
-  #  print(img8)
- #   print(img4)
-    #svr1 = np.divide(((1 - img11)**2), (2 * img11))
-    #svr2 = np.divide(((1 - img12)**2), (2 * img12))
-    print('done')
-  #  save_plot(svr1, svr2, ndvi, ii)
-   # ii += 1
-    # print(ii)
     ndvi = np.reshape(ndvi, -1)
-#    print(ndvi)
-    svr1 = np.reshape(svr1, -1)
     svr2 = np.reshape(svr2, -1)
-    df_ndvi = pd.DataFrame(ndvi, columns = ['ndvi'])
-    df_svr1 = pd.DataFrame(svr1, columns = ['svr1'])
-    df_svr2 = pd.DataFrame(svr2, columns = ['svr1'])
+    final_svr = np.concatenate((final_svr, svr2))
+    final_ndvi = np.concatenate((final_ndvi, ndvi))
     ii += 1
     print(ii)
-    try:
-        quantile_reg(df_ndvi, df_svr2)
-    except ValueError as e:
-        print("******")
-        print(e)
-        continue
-   # ii += 1
-  #  print(ii)
+
+ind1 = np.argwhere(np.isnan(final_svr) | np.isinf(final_svr))
+ind2 = np.argwhere(np.isnan(final_ndvi) | np.isinf(final_ndvi))
+ind = np.unique(np.concatenate((ind1, ind2)))
+print("***********\n", ind.shape)
+df_ndvi = pd.DataFrame(final_ndvi, columns=['ndvi'])
+df_svr = pd.DataFrame(final_svr, columns=['svr'])
+
+quantile_reg(df_ndvi, df_svr)
