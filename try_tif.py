f91b15884dfedf717a5d62c984ceace9d655bd07
NirliptaPande <nirliptapande@gmail.com>
2021-10-01 16:29:36 +0530

    Second tif.py to use the pre-created svr data

---
+++ try_tif.py
@@ -0,0 +1,53 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+plt.rcParams.update({'font.size': 160})
+
+row = 10980
+col = 10980
+
+# len_row = (row // 300) + 1
+# len_col = (col // 300) + 1
+
+c = 0
+for dummy in range(35):
+    data = np.zeros((row, col))
+
+    for len1 in range(0, row, 300):
+        for len2 in range(0, col, 300):
+            print(len1, '\t', len2)
+            test_len1 = 0
+            test_len2 = 0
+            if len1 + 300 > row:
+                test_len1 = row
+            else:
+                test_len1 = len1 + 300
+            if len2 + 300 > col:
+                test_len2 = col
+            else:
+                test_len2 = len2 + 300
+
+            wet_final = np.load('vars/wet_%d_%d.npy' % (len1, len2))
+            wet_final = np.array_split(wet_final, 35)
+            wet = np.reshape(wet_final[dummy], ((test_len1 - len1), (test_len2 - len2)))
+            del wet_final
+            dry_final = np.load('vars/dry_%d_%d.npy' % (len1, len2))
+            dry_final = np.array_split(dry_final, 35)
+            dry = np.reshape(dry_final[dummy], ((test_len1 - len1), (test_len2 - len2)))
+            del dry_final
+            svr_final = np.load('vars/svr_%d_%d.npy' % (len1, len2))
+            svr_final = np.array_split(svr_final, 35)
+            svr = np.reshape(svr_final[dummy], ((test_len1 - len1), (test_len2 - len2)))
+            del svr_final
+
+            soil_data = (svr - dry) / (wet - dry)
+            data[len1:test_len1, len2:test_len2] = soil_data
+
+    del soil_data, wet, dry, img12, svr, n1, n2
+    print("\n")
+    c = c + 1
+    fig = plt.figure(figsize=(109.8, 109.8), dpi=100)
+    plt.imshow(data)
+    plt.colorbar()
+    plt.savefig("soil%s.tiff' % file3[-12:-4]")
+    del data
